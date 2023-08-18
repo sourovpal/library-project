bcc9a45a19cffb4ab24294ecfff98bc7650868f7
sourov pal <sourovpal35@gmail.com>
2023-08-18 06:55:14 +0600

    mdb

--- resources/views/frontend/pages/dashboard.blade.php
+++ resources/views/frontend/pages/dashboard.blade.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-    <link href="https://cdnjs.cloudflare.com/ajax/libs/mdb-ui-kit/6.4.1/mdb.min.css" rel="stylesheet"/>
+    <link href="{{asset('mdb.min.css')}}" rel="stylesheet"/>
 </head>
 <body>
 <br><br><br>
